0208a0428053a52c454b49371bd53b4042abcda1
andresheredia1 <baherediah@gmail.com>
2024-08-28 12:23:59 +0200

    modified config_logging

--- src/hydroBayesCal/utils/config_logging.py
+++ src/hydroBayesCal/utils/config_logging.py
@@ -4,26 +4,44 @@ Setup logging
 import os
 import logging
 
-def setup_logging():
-    # directories
-    script_dir = os.getcwd()
+# directories
+SCRIPT_DIR = os.getcwd() + os.sep
 
-    # Set up logging
-    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-
-    logger = logging.getLogger("HydroBayesCal")
-    logger.setLevel(logging.INFO)
-
-    # Check if handlers are already added to the logger
-    if not logger.handlers:
-        # Create console handler and set level to INFO
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(logging.INFO)
-        console_handler.setFormatter(formatter)
-        logger.addHandler(console_handler)
-
-        # Create file handler for all levels and set mode to append
-        file_handler = logging.FileHandler(os.path.join(script_dir, "logfile.log"), mode="a")
-        file_handler.setLevel(logging.INFO)
-        file_handler.setFormatter(formatter)
-        logger.addHandler(file_handler)
+# setup logging
+info_formatter = logging.Formatter("%(asctime)s - %(message)s")
+warn_formatter = logging.Formatter("WARNING [%(asctime)s]: %(message)s")
+error_formatter = logging.Formatter("ERROR [%(asctime)s]: %(message)s")
+logger = logging.getLogger("HyBayesCal")
+logger.setLevel(logging.INFO)
+logger_warn = logging.getLogger("warnings")
+logger_warn.setLevel(logging.WARNING)
+logger_error = logging.getLogger("errors")
+logger_error.setLevel(logging.ERROR)
+# create console handler and set level to info
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_handler.setFormatter(info_formatter)
+logger.addHandler(console_handler)
+console_whandler = logging.StreamHandler()
+console_whandler.setLevel(logging.WARNING)
+console_whandler.setFormatter(warn_formatter)
+logger_warn.addHandler(console_whandler)
+console_ehandler = logging.StreamHandler()
+console_ehandler.setLevel(logging.ERROR)
+console_ehandler.setFormatter(error_formatter)
+logger_error.addHandler(console_ehandler)
+# create info file handler and set level to debug
+info_handler = logging.FileHandler(SCRIPT_DIR + "logfile.log", "w")
+info_handler.setLevel(logging.INFO)
+info_handler.setFormatter(info_formatter)
+logger.addHandler(info_handler)
+# create warning file handler and set level to error
+warn_handler = logging.FileHandler(SCRIPT_DIR + "warnings.log", "w")
+warn_handler.setLevel(logging.WARNING)
+warn_handler.setFormatter(warn_formatter)
+logger_warn.addHandler(warn_handler)
+# create error file handler and set level to error
+err_handler = logging.FileHandler(SCRIPT_DIR + "errors.log", "w")
+err_handler.setLevel(logging.ERROR)
+err_handler.setFormatter(error_formatter)
+logger_error.addHandler(err_handler)
